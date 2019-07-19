0a08edbffecf44a2579d8a552c1a2d6219c31527
jsadler2 <jeffrey.sadler2@gmail.com>
2019-07-19 16:18:06 -0500

    [#3071] pseudo code for linking with schema doc

--- hs_modelinstance/models.py
+++ hs_modelinstance/models.py
@@ -45,6 +45,17 @@ class ExecutedBy(AbstractMetaDataElement):
         # ExecutedBy element is not repeatable
         unique_together = ("content_type", "object_id")
 
+    @classmethod
+    def create(cls, **kwargs):
+        # pseudo code
+        # 1. check if there is a model_url in the kwargs
+        # 2. check if the model_url is a HS resource or a logical file
+        # 3. if it's an HS resource or logical file check if there is a 'metadata_schema.json' file
+        # 4. if there is a schema doc, add extra metadata terms to the logical file or the resource
+        #    for all of the terms in the doc. The key for these key/value pairs will be the terms
+        #    in the doc. The 'value' will be a blank string that the user will have to fill out for
+        #    now. Maybe later we will have some automatic population.
+        return super(ExecutedBy,cls).create(**kwargs)
 
 # Model Instance Resource type
 class ModelInstanceResource(BaseResource):
